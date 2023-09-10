142be016604566c0ce548999ddee3bf86075c66b
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 <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>
2023-09-10 08:43:46 +0200

    before some changes after break

--- Sources/SwiftUIMetalKit/SwiftUI/MetalViewRepresentables.swift
+++ Sources/SwiftUIMetalKit/SwiftUI/MetalViewRepresentables.swift
@@ -1,65 +1,65 @@
-//
-//  MetalViewRepresentables.swift
-//  
-//
-//  Created by Pirita Minkkinen on 8/22/23.
-//
+    //
+    //  MetalViewRepresentables.swift
+    //
+    //
+    //  Created by Pirita Minkkinen on 8/22/23.
+    //
 
-import SwiftUI
+    import SwiftUI
 
-#if os(macOS)
-public struct MetalNSViewRepresentable: NSViewRepresentable {
+    #if os(macOS)
+    public struct MetalNSViewRepresentable: NSViewRepresentable {
 
-    public typealias NSViewType = MetalElement
-    
-    let viewSize: CGSize
-    let fragmentShaderName: String
-    let shouldScaleByDimensions: Bool
-    
-    public init(viewSize: CGSize, fragmentShaderName: String, shouldScaleByDimensions: Bool) {
-        self.viewSize = viewSize
-        self.fragmentShaderName = fragmentShaderName
-        self.shouldScaleByDimensions = shouldScaleByDimensions
+        public typealias NSViewType = MetalElement
+        
+        let viewSize: CGSize
+        let fragmentShaderName: String
+        let shouldScaleByDimensions: Bool
+        
+        public init(viewSize: CGSize, fragmentShaderName: String, shouldScaleByDimensions: Bool) {
+            self.viewSize = viewSize
+            self.fragmentShaderName = fragmentShaderName
+            self.shouldScaleByDimensions = shouldScaleByDimensions
+        }
+
+        public func makeNSView(context: Context) -> NSViewType {
+            let metalElement = MetalElement(fragmentShaderName: fragmentShaderName, shouldScaleByDimensions: shouldScaleByDimensions)
+            metalElement.viewWidth = Int(viewSize.width)
+            metalElement.viewHeight = Int(viewSize.height)
+            metalElement.commonInit()
+            return metalElement
+        }
+
+        public func updateNSView(_ nsView: NSViewType, context: Context) {
+            // Update the MetalElement view with any required data or state
+            // For this example, we're leaving it empty as there might be no updates necessary.
+        }
+     
+    }
+    #else
+    public struct MetalUIViewRepresentable: UIViewRepresentable {
+        let viewSize: CGSize
+        let fragmentShaderName: String
+        let shouldScaleByDimensions: Bool
+        public typealias NSViewType = MetalElement
+        
+        public init(viewSize: CGSize, fragmentShaderName: String, shouldScaleByDimensions: Bool) {
+            self.viewSize = viewSize
+            self.fragmentShaderName = fragmentShaderName
+            self.shouldScaleByDimensions = shouldScaleByDimensions
+        }
+
+        public func makeUIView(context: Context) -> NSViewType {
+            let metalElement = MetalElement(fragmentShaderName: fragmentShaderName)
+            metalElement.viewWidth = Int(viewSize.width)
+            metalElement.viewHeight = Int(viewSize.height)
+            metalElement.shouldScaleByDimensions = shouldScaleByDimensions
+            metalElement.commonInit()
+            return metalElement
+        }
+        
+      //updateuiveiw missing, idk if it will be needed or not
+        
     }
 
-    public func makeNSView(context: Context) -> NSViewType {
-        let metalElement = MetalElement(fragmentShaderName: fragmentShaderName, shouldScaleByDimensions: shouldScaleByDimensions)
-        metalElement.viewWidth = Int(viewSize.width)
-        metalElement.viewHeight = Int(viewSize.height)
-        metalElement.commonInit()
-        return metalElement
-    }
-
-    public func updateNSView(_ nsView: NSViewType, context: Context) {
-        // Update the MetalElement view with any required data or state
-        // For this example, we're leaving it empty as there might be no updates necessary.
-    }
- 
-}
-#else
-public struct MetalUIViewRepresentable: UIViewRepresentable {
-    let viewSize: CGSize
-    let fragmentShaderName: String
-    let shouldScaleByDimensions: Bool
-    public typealias NSViewType = MetalElement
-    
-    public init(viewSize: CGSize, fragmentShaderName: String, shouldScaleByDimensions: Bool) {
-        self.viewSize = viewSize
-        self.fragmentShaderName = fragmentShaderName
-        self.shouldScaleByDimensions = shouldScaleByDimensions
-    }
-
-    public func makeUIView(context: Context) -> NSViewType {
-        let metalElement = MetalElement(fragmentShaderName: fragmentShaderName)
-        metalElement.viewWidth = Int(viewSize.width)
-        metalElement.viewHeight = Int(viewSize.height)
-        metalElement.shouldScaleByDimensions = shouldScaleByDimensions
-        metalElement.commonInit()
-        return metalElement
-    }
-    
-  //updateuiveiw missing, idk if it will be needed or not
-    
-}
-
-#endif
+    #endif
