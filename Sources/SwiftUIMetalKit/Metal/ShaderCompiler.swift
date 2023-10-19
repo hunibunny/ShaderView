50c248570cdc4ff3f1fe36946f8435e7633f0d4a
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 <piritaminkkinen@gmail.com>
2023-10-19 20:08:57 +0200

    might have fixed the non compiling ooo

--- Sources/SwiftUIMetalKit/Metal/ShaderCompiler.swift
+++ Sources/SwiftUIMetalKit/Metal/ShaderCompiler.swift
@@ -23,10 +23,41 @@ class ShaderCompiler {
     }
     
     func compileShaderAsync(_ source: String, key: String, completion: @escaping (Result<MTLFunction, ShaderCompilationError>) -> Void) {
+        
+        // Print the entire shader source code
+        print("Shader source code for key \(key):\n\(source)")
+        
+        print("compile started")
+        
+        DispatchQueue.global().async {
+            os_log("Attempting to create shader with key: %{PUBLIC}@", log: OSLog.default, type: .debug, key)
+            
+            // Assuming you have access to the MTLDevice as 'device'
+            do {
+                let shaderLibrary = try self.device.makeLibrary(source: source, options: nil)
+                guard let shaderFunction = shaderLibrary.makeFunction(name: key) else {
+                    os_log("Failed to create shader with key: %{PUBLIC}@", log: OSLog.default, type: .error, key)
+                    completion(.failure(.functionCreationFailed("Failed to create shader function with key: \(key)")))
+                    return
+                }
+                os_log("Successfully created a shader with key: %{PUBLIC}@", log: OSLog.default, type: .debug, key)
+                completion(.success(shaderFunction))
+            } catch let error {
+                os_log("Failed to create shader library for key: %{PUBLIC}@ due to error: %{PUBLIC}@", log: OSLog.default, type: .error, key, error.localizedDescription)
+                completion(.failure(.functionCreationFailed("Failed to create shader library for key: \(key) due to error: \(error.localizedDescription)")))
+            }
+        }
+    }
+
+
+    
+    /*
+    func compileShaderAsync(_ source: String, key: String, completion: @escaping (Result<MTLFunction, ShaderCompilationError>) -> Void) {
+        print("Shader source code for key \(key):\n\(source)")
         print("compile started")
         DispatchQueue.global().async {
             os_log("Attempting to create shader with key: %{PUBLIC}@", log: OSLog.default, type: .debug, key)
-            guard let shaderFunction = self.library.makeFunction(name: key) else {
+            guard let shaderFunction = self.library.makeFunction(source: source) else {
                 os_log("Failed to create shader with key: %{PUBLIC}@", log: OSLog.default, type: .error, key)
                 completion(.failure(.functionCreationFailed("Failed to create shader function with key: \(key)")))
                 return
@@ -34,7 +65,7 @@ class ShaderCompiler {
             os_log("Successfully created a shader with key: %{PUBLIC}@", log: OSLog.default, type: .debug, key)
             completion(.success(shaderFunction))
         }
-/*
+
         DispatchQueue.global().async {
             guard let shaderFunction = self.library.makeFunction(name: key) else {
                 completion(.failure(.functionCreationFailed("Failed to create shader function with key: \(key)")))
