1e4eaab13231ab3b7c8cd1f770a36b907d9ec566
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 <piritaminkkinen@gmail.com>
2023-12-05 18:12:38 +0100

    pollished shaderView

---
+++ Sources/ShaderView/SwiftUI/MetalViewRepresentable.swift
@@ -0,0 +1,26 @@
+//
+//  MetalViewRepresentable.swift
+//  
+//
+//  Created by Pirita Minkkinen on 12/5/23.
+//
+
+import Foundation
+import SwiftUI
+import MetalKit
+
+// Define a struct that conditionally returns the correct view for the platform.
+struct MetalViewRepresentable: View {
+    var drawableSize: CGSize
+    var shaderViewModel: ShaderViewModel
+
+    var body: some View {
+        #if os(macOS)
+        // macOS specific view
+        MetalNSViewRepresentable(drawableSize: drawableSize, shaderViewModel: shaderViewModel)
+        #else
+        // iOS specific view
+        MetalUIViewRepresentable(drawableSize: drawableSize, shaderViewModel: shaderViewModel)
+        #endif
+    }
+}
--- Sources/ShaderView/SwiftUI/ShaderView.swift
+++ Sources/ShaderView/SwiftUI/ShaderView.swift
@@ -17,10 +17,10 @@ import MetalKit
 /// It provides a fallback and placeholder view for different states of shader loading.
 public struct ShaderView: View {
     @ObservedObject var shaderViewModel: ShaderViewModel
-    let fragmentShaderName: String
-    let vertexShaderName: String
-    var usingDefaultShaders: Bool = true
+    
     @State var shadersLoaded: Bool = false
+    var usingDefaultShaders: Bool = true
+    
     let fallbackView: AnyView
     let placeholderView: AnyView
     
@@ -34,26 +34,17 @@ public struct ShaderView: View {
     public init(fragmentShaderName: String? = nil, vertexShaderName: String? = nil, fallbackView: AnyView? = nil, placeholderView: AnyView? = nil, shaderInput: (any ShaderInputProtocol)? = nil) {
         self.fallbackView = fallbackView ?? AnyView(FallbackView())
         self.placeholderView = placeholderView ?? AnyView(PlaceholderView())
-        
+
         // Setup shader names and determine if default shaders are used.
-        if let name = fragmentShaderName {
-            self.fragmentShaderName = name
-            usingDefaultShaders = false
-        } else {
-            self.fragmentShaderName = "defaultFragmentShader"
-        }
-        
-        if let name = vertexShaderName {
-            self.vertexShaderName = name
-            
-        } else {
-            self.vertexShaderName = "defaultVertexShader"
-            usingDefaultShaders = true
-        }
+        let finalFragmentShaderName = fragmentShaderName ?? "defaultFragmentShader"
+        let finalVertexShaderName = vertexShaderName ?? "defaultVertexShader"
+               
+        let usingDefaultShaders = fragmentShaderName == nil && vertexShaderName == nil
+               
         
         // Initialize the shader view model with the shader names and input.
-        self.shaderViewModel = ShaderViewModel(vertexShaderName: self.vertexShaderName, fragmentShaderName: self.fragmentShaderName, shaderInput: shaderInput ?? ShaderInput())
-        
+        self.shaderViewModel = ShaderViewModel(vertexShaderName: finalVertexShaderName, fragmentShaderName: finalFragmentShaderName, shaderInput: shaderInput ?? ShaderInput())
+
         // TODO: This is fine until adding loading and add real-time compilation for user shaders.
         if(!usingDefaultShaders){
             shaderViewModel.viewState = .metalView;
@@ -62,45 +53,22 @@ public struct ShaderView: View {
     }
   
     
-    
     public var body: some View {
         GeometryReader { geometry in
-            if shadersLoaded { // Check if shaders have already been loaded
-                // Display the Metal view since shaders have been loaded
-#if os(macOS)
-                
-                MetalNSViewRepresentable(drawableSize: geometry.size, shaderViewModel: shaderViewModel)
-#else
-                MetalUIViewRepresentable(drawableSize: geometry.size, shaderViewModel: shaderViewModel)
-#endif
+            switch shaderViewModel.viewState {
+            case .metalView:
+                MetalViewRepresentable(drawableSize: geometry.size, shaderViewModel: shaderViewModel)
+            case .placeholder:
+                placeholderView.frame(width: geometry.size.width, height: geometry.size.height)
+            case .error:
+                fallbackView.frame(width: geometry.size.width, height: geometry.size.height)
             }
-            else{
-                switch shaderViewModel.viewState {
-                case .placeholder:
-                    placeholderView
-                        .frame(width: geometry.size.width, height: geometry.size.height)
-                case .metalView:
-                    
-#if os(macOS)
-                    MetalNSViewRepresentable(drawableSize: geometry.size, shaderViewModel: shaderViewModel)
-                
-#else
-                    MetalUIViewRepresentable(drawableSize: geometry.size, shaderViewModel: shaderViewModel)
-               
-#endif
-                    
-                case .error:
-                    fallbackView
-                        .frame(width: geometry.size.width, height: geometry.size.height)
-                }}
         }
-        
         .onChange(of: shaderViewModel.viewState) { newState in
-            if newState == .metalView {
-                shadersLoaded = true
-            }
+            shadersLoaded = newState == .metalView
         }
     }
+
 }
 
 
